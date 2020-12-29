37ad053e803f67ce4a54374e972a4017fbc65245
Henry <henrygabrielgm@gmail.com>
2020-12-29 19:06:35 -0400

    modified the insert book button

--- script.js
+++ script.js
@@ -8,8 +8,24 @@ function Book (name,author,pages,read) {
 function addBookToLibrary (book) {
     myLibrary.push(book);
 }
+function showBookLibrary (array) {
+        var div = document.createElement('div');
+        div.textContent = 'hello';
+        shelf.appendChild(div);
+}
 
-const form = document.querySelectorAll('#form');
+const shelf = document.querySelector('#shelf');
+const form = document.querySelector('#form');
+const submit = document.querySelector('#submit');
 
-console.log(form);
+submit.addEventListener('click', () => {
+    var name = document.getElementById("name").value;
+    var author = document.getElementById("author").value;
+    var pages = document.getElementById("pages").value;
+    var read = document.getElementById('yes').checked;
+    var book = new Book(name,author,pages,read);
+    addBookToLibrary(book);
+    console.log(myLibrary);
+    showBookLibrary(myLibrary);
 
+});

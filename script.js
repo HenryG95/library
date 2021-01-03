c15bc3279356a943da846d34f1ec6308b6564bd7
Henry <henrygabrielgm@gmail.com>
2021-01-03 14:52:13 -0400

    updates

--- script.js
+++ script.js
@@ -9,14 +9,43 @@ function addBookToLibrary (book) {
     myLibrary.push(book);
 }
 function showBookLibrary (array) {
-        var div = document.createElement('div');
-        div.textContent = 'hello';
-        shelf.appendChild(div);
-}
+        array.forEach(item => {
+            var div = document.createElement('div');
+            div.setAttribute('class','item');
+            div.innerHTML = `<br><h3>${item.name}</h3><br><h3>${item.author}</h3><br>
+                <h3>${item.pages} Pages</h3><br>`;
+            var buttonDiv = document.createElement('button');
+            if(item.read == true) {
+                buttonDiv.textContent = 'Read';
+            } else {
+                buttonDiv.textContent = 'Not Read';
+                div.style['background-color'] = 'wheat';
+                div.style['color'] ='firebrick';
+            }
+            var eraseDiv = document.createElement('button');
+            eraseDiv.textContent= 'Erase';
+            div.appendChild(buttonDiv);
+            div.appendChild(eraseDiv);
+            shelf.appendChild(div);
+            shelf.appendChild(formContainer);
+            shelf.appendChild(newBook);
+            eraseDiv.addEventListener('click', () => {
+                shelf.removeChild(div);
+            })
+            buttonDiv.addEventListener('click', () => {
+                buttonDiv.textContent = 'Read';
+                div.style['background-color'] = 'firebrick';
+                div.style['color'] ='wheat';
 
+            })
+        })
+}
+const container = document.querySelector('#container');
+const formContainer = document.querySelector('#form-container');
 const shelf = document.querySelector('#shelf');
 const form = document.querySelector('#form');
 const submit = document.querySelector('#submit');
+const newBook = document.querySelector('#new');
 
 submit.addEventListener('click', () => {
     var name = document.getElementById("name").value;
@@ -27,5 +56,10 @@ submit.addEventListener('click', () => {
     addBookToLibrary(book);
     console.log(myLibrary);
     showBookLibrary(myLibrary);
+    formContainer.style.display = 'none';
 
 });
+
+newBook.addEventListener('click', () => {
+    formContainer.style.display = 'block';
+})
